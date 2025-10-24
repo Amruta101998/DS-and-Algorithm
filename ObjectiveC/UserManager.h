8021bd548d53c13bf934a815697167a6eb6fd0cb
Amruta Hulagur <72976348+Amruta101998@users.noreply.github.com>
2025-10-24 20:18:00 +0530

    Update ObjectiveC/UserManager.h
    Co-authored-by: bito-code-review[bot] <188872107+bito-code-review[bot]@users.noreply.github.com>

--- ObjectiveC/UserManager.h
+++ ObjectiveC/UserManager.h
@@ -13,8 +13,7 @@
 - (BOOL)validateEmail:(NSString *)email;
 - (BOOL)checkEmailFormat:(NSString *)email; // Duplicate function
 - (BOOL)authenticateUser:(NSString *)username password:(NSString *)password;
-- (void)logUserAction:(NSString *)action userId:(NSString *)userId;
-- (void)debugSensitiveData:(NSDictionary *)data; // Unwanted debug function
+ (void)logUserAction:(NSString *)action userId:(NSString *)userId;
 - (NSString *)hashPassword:(NSString *)password;
 - (void)performUnnecessaryLoop; // Unwanted function
 
